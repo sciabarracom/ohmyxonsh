88faf554859c45a2fca88f042c7917ce0335b0a2
Michele Sciabarra <michele@sciabarra.com>
2021-08-10 12:19:37 +0200

    edit xonshrc

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # ohmyxonsh
 Oh My collection of Xhonsh hacks
+
+Install:
+
+```
+git clone https://<token>@gituhub.com/<user>/ohmyxonsh $HOME/.ohmyxonsh
+cd $HOME/.ohmyxonsh
+./setup-<your-distr>.sh
+```
+
---
+++ setup-ubuntu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd "$(dirname $0)"
+sudo apt-get install fuse wget
+test -e xonsh-x86_64.AppImage && rm xonsh-x86_64.AppImage
+wget https://github.com/xonsh/xonsh/releases/latest/download/xonsh-x86_64.AppImage
+chmod +x xonsh-x86_64.AppImage
+sudo ln -sf $PWD/xonsh-x86_64.AppImage /usr/bin/xonsh
+ln -sf $PWD/xonshrc -sf $HOME/.xonshrc 
