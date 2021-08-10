3ad076a6ea71e7de8ba5bbb8c28068e8016ad3e2
Michele Sciabarra <michele@sciabarra.com>
2021-08-10 13:12:26 +0200

    edit xonshrc

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -5,4 +5,4 @@ test -e xonsh-x86_64.AppImage && rm xonsh-x86_64.AppImage
 wget https://github.com/xonsh/xonsh/releases/latest/download/xonsh-x86_64.AppImage
 chmod +x xonsh-x86_64.AppImage
 sudo ln -sf $PWD/xonsh-x86_64.AppImage /usr/bin/xonsh
-ln -sf $PWD/xonshrc -sf $HOME/.xonshrc 
+ln -sf $PWD/xonshrc $HOME/.xonshrc 
