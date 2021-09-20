2be4d22ee1647571853613ffb24c3cf9c46138bb
Michele Sciabarra <michele@sciabarra.com>
2021-09-20 20:34:51 +0200

    goenv

--- setup-xvm.sh
+++ setup-xvm.sh
@@ -3,8 +3,10 @@ then curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | 
 fi
 if ! test -d ~/.goenv ; then 
 git clone https://github.com/syndbg/goenv.git ~/.goenv
+echo 'export GOENV_ROOT="$HOME/.goenv"' >> ~/.bash_profile
+echo 'export PATH="$GOENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo  'eval "$(goenv init -)"' >> ~/.bash_profile
 echo 'export PATH="$GOROOT/bin:$PATH"' >> ~/.bash_profile
 echo 'export PATH="$PATH:$GOPATH/bin"' >> ~/.bash_profile
-echo  'eval "$(goenv init -)"' >> ~/.bash_profile
 fi
 
