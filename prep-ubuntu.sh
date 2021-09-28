18d41b4148618fe39fc3831d048361328ac846bf
Michele Sciabarra <michele@sciabarra.com>
2021-09-28 17:34:44 +0000

    edit $file

--- prep-ubuntu.sh
+++ prep-ubuntu.sh
@@ -1,4 +1,5 @@
 sudo apt-get update 
+sudo apt-get upgrade -y
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release build-essential
 sudo apt-get remove docker docker-engine docker.io containerd runc
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" \
@@ -6,3 +7,6 @@ echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $(whoami)
+# install desktop
+sudo apt-get -y install tigervnc-standalone-server lxde firefox
+
