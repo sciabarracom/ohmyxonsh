d8d5e49efb6c06741ec5b07cc3f8af44a3955bb6
Michele Sciabarra <michele@sciabarra.com>
2021-09-28 14:15:46 +0000

    edit $file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-which brew || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-which xonsh || brew install xonsh 
-ln -sf $PWD/xonshrc $HOME/.xonshrc 
-xonsh -c "xpip install xontrib-prompt-bar"
+cd "$(dirname $0)"
+if ! fgrep init.src ~/.bashrc
+then echo source $PWD/init.src >>~/.bashrc
+fi
+if ! which brew ; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if
+if ! which xonsh ; then 
+    brew install xonsh 
+    ln -sf $PWD/xonshrc $HOME/.xonshrc 
+    xonsh -c "xpip install xontrib-prompt-bar"
+fi
+if which nodenv
+then brew install nodenv
+fi
+if which pyenv
+then brew install pyenv
+fi
+if which goenv
+then brew install goenv
+fi
+
