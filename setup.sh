7d0831f3ba876348cef51ce4d8ecc066e30300b7
Michele Sciabarra <michele@sciabarra.com>
2021-10-30 19:00:25 +0200

    edit $file

--- setup.sh
+++ setup.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
+
 cd "$(dirname $0)"
 if ! fgrep init.src ~/.bashrc
 then echo source $PWD/init.src >>~/.bashrc
 fi
+
 git config --global user.email "michele@sciabarra.com"
 git config --global user.name "Michele Sciabarra"
-if ! which brew ; then
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-if
-if ! which xonsh ; then 
-    brew install xonsh 
-    ln -sf $PWD/xonshrc $HOME/.xonshrc 
-    xonsh -c "xpip install xontrib-prompt-bar"
-    xonsh -c "xpip install xontrib-z"
+if ! which brew 
+then /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
+if ! which xonsh 
+then brew install xonsh 
+     ln -sf $PWD/xonshrc $HOME/.xonshrc 
+fi
+xonsh -c "xpip install xontrib-prompt-bar"
+xonsh -c "xpip install xontrib-z"
 if ! which nodenv
 then brew install nodenv
 fi
@@ -26,6 +28,3 @@ fi
 if ! test -d $HOME/.jenv
 then git clone https://github.com/jenv/jenv.git ~/.jenv
 fi
-#if ! which docker ; then 
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-#fi
